9a0b5e792066ad8e6ac0412ba8b86aadc9bf517b
mandaradd28 <mandaradd14@gmail.com>
2025-04-13 12:33:27 +0530

    first commit

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vite.dev/config/
 export default defineConfig({
-  base: '/DesiDelights/', // 👈 This is essential for GitHub Pages
   plugins: [react()],
 })
